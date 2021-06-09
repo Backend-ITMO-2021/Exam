9c0200bdc814da5c0c5591792914d01488fea5c7
Publish - Max - gork <maxim.uch27@mail.ru>
2021-06-09 16:48:16 +0300

    Project started

---
+++ springchat/src/test/java/com/exam/springchat/SpringchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.springchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
